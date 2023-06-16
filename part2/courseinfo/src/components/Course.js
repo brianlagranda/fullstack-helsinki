beecd38ae78a19c891416d56407bbb933da33684
bra20019613 <brianlagranda@gmail.com>
2023-06-16 16:01:02 -0300

    Exercises 2.1.-2.5. Done

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,37 @@
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+const Header = ({ course }) => <h2>{course.name}</h2>;
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </div>
+  );
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((s, p) => s + p.exercises, 0);
+
+  return <b>Number of exercises {total}</b>;
+};
+
+export default Course;
