fd0bf8367d219e65db5c6bc586eb5dcf28da3145
bra20019613 <brianlagranda@gmail.com>
2023-08-07 20:03:23 -0300

    Exercise 5.1 - Done

---
+++ part5/bloglist-frontend/src/components/Notification.js
@@ -0,0 +1,10 @@
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null;
+  }
+
+  const style = message.toLowerCase().includes("error") ? "error" : "success";
+  return <div className={style}>{message}</div>;
+};
+
+export default Notification;
